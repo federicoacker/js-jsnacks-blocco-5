d466a1a09608a43080de4fbbaced924aeee0e85e
federicoacker <acker.federico@gmail.com>
2026-04-14 14:53:20 +0200

    Fatti i primi 2 snack

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,4 +1,6 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
 
+names.forEach((name) => {console.log(name);}); // Per ogni elemento dell'array, che ho chiamato name 
+// esegui la funzione di callback che fa il console.log(name)
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
--- snack-2/script.js
+++ snack-2/script.js
@@ -4,5 +4,6 @@ const people = [
   { name: 'Marco', age: 67 }
 ];
 
+people.forEach((person) => {console.log(person.name)}); // Di nuovo, per ogni oggetto dell'array, eseguo la callback che fa la stampa del nome
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
