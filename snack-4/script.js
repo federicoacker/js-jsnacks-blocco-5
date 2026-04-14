a4e601c75f1f76772ff2397a4c0124d3670670b0
federicoacker <acker.federico@gmail.com>
2026-04-14 15:00:12 +0200

    Fatto snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -5,5 +5,10 @@ const posts = [
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
 
+const authorNames = posts.map((post) => post.author); // Map con una callback che semplicemente
+// prende ogni elemento e ne restituisce la proprietà name che poi viene aggiunta all'array interno della funzione map, che una volta finito
+// restituisce l'array ad authorNames
+
+console.log(authorNames);
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
